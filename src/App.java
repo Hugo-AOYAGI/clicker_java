95d178afd307f78c115a15d16ece8dfbd383b843
Hugo-AOYAGI <aoyagihugo@gmail.com>
2021-11-22 17:30:11 +0100

    test commit

--- src/App.java
+++ src/App.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import org.eclipse.swt.*;
 import org.eclipse.swt.widgets.*;
 
@@ -19,23 +18,5 @@ public class App {
 
         display.dispose();
 
-=======
-import org.eclipse.swt.widgets.*;
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.*;
-import org.eclipse.swt.events.*;
-
-public class App {
-    
-    public static void centrerSurEcran(Display display, Shell shell) {
-		Rectangle rect = display.getClientArea();
-		Point size = shell.getSize();
-		int x = (rect.width - size.x)/2;
-		int y = (rect.height - size.y)/2;
-		shell.setLocation(new Point(x,y));
->>>>>>> 7c1536197a7594e8369cc88518a29ebfb3f1e729
     }
-    public static void main(String[] args) {
-        
-	};
 }
